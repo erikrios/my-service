7463f30a5a5b4ae174c88f8ecf5b769e937a8c75
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-04 19:33:14 +0700

    Bind the view

--- app/src/main/java/io/erikrios/github/myservice/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myservice/MainActivity.kt
@@ -2,10 +2,31 @@ package io.erikrios.github.myservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStartService: Button = findViewById(R.id.btn_start_service)
+        btnStartService.setOnClickListener {
+
+        }
+
+        val btnStartJobIntentService: Button = findViewById(R.id.btn_start_job_intent_service)
+        btnStartJobIntentService.setOnClickListener {
+
+        }
+
+        val btnStartBoundService: Button = findViewById(R.id.btn_start_bound_service)
+        btnStartBoundService.setOnClickListener {
+
+        }
+
+        val btnStopBoundService: Button = findViewById(R.id.btn_stop_bound_service)
+        btnStopBoundService.setOnClickListener {
+
+        }
     }
 }
